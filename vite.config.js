4591d15940e8a57c559218440b6464c91a7302a1
Vinícius Hoyer <contact@vhoyer.dev>
2021-07-06 19:17:13 -0300

    :wrench: Configure vite aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import { ViteAliases } from 'vite-aliases'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    ViteAliases(),
+    vue(),
+  ]
 })
