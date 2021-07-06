3d2e3cd48d227f1c8201f099327a3f13d9cc7dc7
Vinícius Hoyer <contact@vhoyer.dev>
2021-07-06 19:17:29 -0300

    :sparkles: Add base for card and hand

---
+++ src/components/Card/Card.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <div v-html="html" />
+  </div>
+</template>
+
+<script>
+import DOMPurify from 'dompurify'
+import { cardType } from '@models/card.js';
+
+export default {
+  name: 'Card',
+  props: {
+    type: {
+      type: Object,
+      required: true,
+    },
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    const type = cardType(props.type)
+
+    return {
+      html: DOMPurify.sanitize(type.render(props.data)),
+    }
+  },
+}
+</script>
+
+<style>
+.card {
+  border-radius: var(--card-border-radius);
+  border: var(--card-border);
+  width: var(--card-width);
+  height: var(--card-height);
+  contain: strict;
+  text-align: center;
+  background-color: white;
+}
+</style>
---
+++ src/components/MyHand/MyHand.vue
@@ -0,0 +1,80 @@
+<template>
+  <div
+    class="my-hand"
+    tabindex="0"
+  >
+    <Card
+      class="my-hand__item"
+      tabindex="0"
+      v-for="data in cardDataList"
+      :type="cardType"
+      :data="data"
+    />
+  </div>
+</template>
+
+<script>
+import Card from '@components/Card';
+
+export default {
+  name: 'MyHand',
+  components: {
+    Card,
+  },
+  setup() {
+    return {
+      cardType: {
+        template: `
+          <div style="">{default}</div>
+        `,
+      },
+      cardDataList: [
+        { default: `1` },
+        { default: `2` },
+        { default: `3` },
+        { default: `4` },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.my-hand {
+  z-index: 0;
+  display: flex;
+  justify-content: center;
+  padding-left: var(--space-m);
+  padding-right: var(--space-m);
+  padding-bottom: var(--space-m);
+  gap: var(--space-m);
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  transform: translateY(75%);
+  transition: transform .3s ease-in;
+}
+
+.my-hand:focus,
+.my-hand:focus-within {
+  transform: translateY(0%);
+}
+
+.my-hand__item {
+  flex-shrink: 0;
+  transition-duration: .2s;
+  transition-timing-function: ease-in;
+  transition-property: transform, box-shadow;
+}
+
+.my-hand__item:not(:first-child) {
+  margin-left: calc(-1 * var(--card-width));
+}
+
+.my-hand__item:hover {
+  z-index: 1;
+  transform: translateY(-10%);
+  box-shadow: 0 0 0 5px black;
+}
+</style>
