4fe5be1847501e5676701951a26675f1ea6307c5
Vinícius Hoyer <contact@vhoyer.dev>
2021-07-08 21:33:45 -0300

    :construction: Prepare a better home screen

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,11 +1,29 @@
 <template>
-  <router-link :to="{ name: 'room' }">
-    Create room
-  </router-link>
+  <input
+    v-model="username"
+    type="text"
+  >
+
+  <button @click="goToRoom">
+    Start
+  </button>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
+  setup() {
+    const username = ref('')
+
+    const goToRoom = () => {
+    }
+
+    return {
+      username,
+      goToRoom,
+    }
+  },
 };
 </script>
