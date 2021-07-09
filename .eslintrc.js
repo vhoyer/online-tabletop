f973042051af170b804b100c0c0747caa59e4742
Vinícius Hoyer <contact@vhoyer.dev>
2021-07-08 22:53:57 -0300

    :rotating_light: Config more rules to linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,9 @@ module.exports = {
     extends: [
         'plugin:vue/vue3-recommended',
     ],
-};
+    rules: {
+        'semi': ['error', 'never'],
+        'quotes': ['error', 'single'],
+        'comma-dangle': ['error', 'always-multiline'],
+    },
+}
