ba69f7e11b1061d516530370e897dc7a013a041e
Vinícius Hoyer <contact@vhoyer.dev>
2021-07-09 00:18:04 -0300

    :art: Fix indentation on Room files

--- src/models/Room.js
+++ src/models/Room.js
@@ -1,20 +1,20 @@
 import deepmerge from 'deepmerge'
 
 export function Room(props = {}) {
-	Object.assign(this, deepmerge({
-		users: {},
-		game: null,
-	}, props))
+  Object.assign(this, deepmerge({
+    users: {},
+    game: null,
+  }, props))
 
-	this.addUser = (username) => {
-		if (!username) throw new Error("User can't have empty username")
-		if (this.users[username]) throw new Error('User already exists in the room')
+  this.addUser = (username) => {
+    if (!username) throw new Error("User can't have empty username")
+    if (this.users[username]) throw new Error('User already exists in the room')
 
-		const type = Object.keys(this.users).length > 0 ? 'spectator' : 'host'
+    const type = Object.keys(this.users).length > 0 ? 'spectator' : 'host'
 
-		this.users[username] = {
-			type,
-			createdAt: new Date(),
-		}
-	}
+    this.users[username] = {
+      type,
+      createdAt: new Date(),
+    }
+  }
 }
--- src/models/__tests__/Room.test.js
+++ src/models/__tests__/Room.test.js
@@ -1,71 +1,71 @@
 import { Room } from '@models/Room'
 
 describe('Models > Room', () => {
-	def('subject', () => new Room($.base))
+  def('subject', () => new Room($.base))
 
-	it('initiates as a empty room', () => {
-		expect($.subject).toEqual({
-			addUser: expect.any(Function),
-			users: {},
-			game: null,
-		})
-	})
+  it('initiates as a empty room', () => {
+    expect($.subject).toEqual({
+      addUser: expect.any(Function),
+      users: {},
+      game: null,
+    })
+  })
 
-	describe('when call addUser with "" (Empty string)', () => {
-		it('initiates as a empty room', () => {
-			expect(() => {
-				$.subject.addUser('')
-			}).toThrow(new Error("User can't have empty username"))
-		})
-	})
+  describe('when call addUser with "" (Empty string)', () => {
+    it('initiates as a empty room', () => {
+      expect(() => {
+        $.subject.addUser('')
+      }).toThrow(new Error("User can't have empty username"))
+    })
+  })
 
-	describe('when call addUser with "o_mago"', () => {
-		beforeEach(() => {
-			$.subject.addUser('o_mago')
-		})
+  describe('when call addUser with "o_mago"', () => {
+    beforeEach(() => {
+      $.subject.addUser('o_mago')
+    })
 
-		it('add first user as host', () => {
-			expect($.subject).toEqual({
-				addUser: expect.any(Function),
-				users: {
-					'o_mago': {
-						createdAt: expect.any(Date),
-						type: 'host',
-					},
-				},
-				game: null,
-			})
-		})
+    it('add first user as host', () => {
+      expect($.subject).toEqual({
+        addUser: expect.any(Function),
+        users: {
+          'o_mago': {
+            createdAt: expect.any(Date),
+            type: 'host',
+          },
+        },
+        game: null,
+      })
+    })
 
-		describe('when call addUser with "bolinha_gamer"', () => {
-			beforeEach(() => {
-				$.subject.addUser('bolinha_gamer')
-			})
+    describe('when call addUser with "bolinha_gamer"', () => {
+      beforeEach(() => {
+        $.subject.addUser('bolinha_gamer')
+      })
 
-			it('Add new user as spectator', () => {
-				expect($.subject).toEqual({
-					addUser: expect.any(Function),
-					users: {
-						'o_mago': {
-							createdAt: expect.any(Date),
-							type: 'host',
-						},
-						'bolinha_gamer': {
-							createdAt: expect.any(Date),
-							type: 'spectator',
-						},
-					},
-					game: null,
-				})
-			})
-		})
+      it('Add new user as spectator', () => {
+        expect($.subject).toEqual({
+          addUser: expect.any(Function),
+          users: {
+            'o_mago': {
+              createdAt: expect.any(Date),
+              type: 'host',
+            },
+            'bolinha_gamer': {
+              createdAt: expect.any(Date),
+              type: 'spectator',
+            },
+          },
+          game: null,
+        })
+      })
+    })
 
-		describe('when call addUser with "o_mago"', () => {
-			it('initiates as a empty room', () => {
-				expect(() => {
-					$.subject.addUser('o_mago')
-				}).toThrow(new Error('User already exists in the room'))
-			})
-		})
-	})
+    describe('when call addUser with "o_mago"', () => {
+      it('initiates as a empty room', () => {
+        expect(() => {
+          $.subject.addUser('o_mago')
+        }).toThrow(new Error('User already exists in the room'))
+      })
+    })
+  })
 })
