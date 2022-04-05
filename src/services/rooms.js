378eb45537593a65f62e62fcc0a689699b7d2f7d
Vinícius Hoyer <contact@vhoyer.dev>
2022-04-05 13:38:57 -0300

    :technologist: Move firebase init to new file so that HMR works better

--- src/services/rooms.js
+++ src/services/rooms.js
@@ -1,20 +1,6 @@
-import firebase from 'firebase/app'
-import 'firebase/database'
+import { database } from './vendor/firebase'
 import { Room } from '@models/Room'
 import { observableDiff } from 'deep-diff'
-
-firebase.initializeApp({
-  apiKey: 'AIzaSyDAjRc0ycZ3RPfjKlYiAyyEP8GkB0fPuco',
-  authDomain: 'online-tabletop-4dc50.firebaseapp.com',
-  databaseURL: 'https://online-tabletop-4dc50-default-rtdb.firebaseio.com',
-  projectId: 'online-tabletop-4dc50',
-  storageBucket: 'online-tabletop-4dc50.appspot.com',
-  messagingSenderId: '206814685311',
-  appId: '1:206814685311:web:6aa4524ee228e8d152f74a',
-  measurementId: 'G-4Z52DVNE4E',
-})
-
-const database = firebase.database()
 
 /**
  * @param {Room} room from @models/Room
---
+++ src/services/vendor/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDAjRc0ycZ3RPfjKlYiAyyEP8GkB0fPuco',
+  authDomain: 'online-tabletop-4dc50.firebaseapp.com',
+  databaseURL: 'https://online-tabletop-4dc50-default-rtdb.firebaseio.com',
+  projectId: 'online-tabletop-4dc50',
+  storageBucket: 'online-tabletop-4dc50.appspot.com',
+  messagingSenderId: '206814685311',
+  appId: '1:206814685311:web:6aa4524ee228e8d152f74a',
+  measurementId: 'G-4Z52DVNE4E',
+})
+
+export { firebase }
+export const database = firebase.database()
